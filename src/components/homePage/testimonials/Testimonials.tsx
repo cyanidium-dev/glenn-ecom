719aa327dfa67a4420fc34f9dd86d8d23043dec6
Shade <kulakaa073@gmail.com>
2026-01-25 23:34:09 +0200

    fix: fixing loader

--- src/components/homePage/testimonials/Testimonials.tsx
+++ src/components/homePage/testimonials/Testimonials.tsx
@@ -1,13 +1,22 @@
 import Container from "@/components/shared/container/Container";
 import { testimonialsData } from "./testimonialsData";
 import TestimonialSwiper from "./TestimonialSwiper";
+import * as motion from "motion/react-client";
+import { fadeInAnimation } from "@/utils/animationVariants";
 
 export default function Testimonials() {
   return (
     <section className="pt-[45px] lg:pb-[100px]">
       <Container className="px-[20px] ssm:px-[20px]">
         <h2 className="sr-only">Testimonials</h2>
-        <TestimonialSwiper testimonialsList={testimonialsData} />
+        <motion.div
+          variants={fadeInAnimation({ y: 50, duration: 1, scale: 0.9 })}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
+          <TestimonialSwiper testimonialsList={testimonialsData} />
+        </motion.div>
       </Container>
     </section>
   );
