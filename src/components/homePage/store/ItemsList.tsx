6f7d5df55a4cda0e2d71f53b19913c6eb69fc8ee
Shade <kulakaa073@gmail.com>
2026-01-26 19:33:31 +0200

    fix: switching to grid to handle more than 2 items

--- src/components/homePage/store/ItemsList.tsx
+++ src/components/homePage/store/ItemsList.tsx
@@ -7,7 +7,7 @@ interface ItemsListProps {
 
 export default function ItemsList({ items }: ItemsListProps) {
   return (
-    <ul className="flex flex-col md:flex-row space-between gap-20 lg:gap-0 items-center justify-center w-full">
+    <ul className="grid grid-cols-1 md:grid-cols-2 gap-20 lg:gap-x-0 lg:gap-y-25 items-center justify-center w-full">
       {items.map(item => (
         <ItemCard key={item.slug} item={item} />
       ))}
