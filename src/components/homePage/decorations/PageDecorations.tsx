14d41fe8c48b5a9409355d118cd485d4e2f39179
Shade <kulakaa073@gmail.com>
2026-01-28 23:46:14 +0200

    fix: fixing overflow issue, fixing typos

--- src/components/homePage/decorations/PageDecorations.tsx
+++ src/components/homePage/decorations/PageDecorations.tsx
@@ -10,14 +10,14 @@ export default function PageDecorations() {
         className="absolute -z-30 top-[-24%] bottom-[26%] lg:top-[-60%] lg:bottom-[-30%] left-1/2 -translate-x-1/2 w-full"
         style={{
           background:
-            "linear-gradient(180deg, #92001D 8.27%, rgba(146, 0, 29, 0) 41.52%);",
+            "linear-gradient(180deg, #92001D 8.27%, rgba(146, 0, 29, 0) 41.52%)",
         }}
       />
       <div
         className="absolute -z-30 top-[-24%] bottom-[26%] lg:top-[-60%] lg:bottom-[-30%] left-1/2 -translate-x-1/2 w-full"
         style={{
           background:
-            "linear-gradient(180deg, rgba(0, 0, 0, 0) 68.92%, #000000 88.35%);",
+            "linear-gradient(180deg, rgba(0, 0, 0, 0) 68.92%, #000000 88.35%)",
         }}
       />
       <div
@@ -28,7 +28,7 @@ export default function PageDecorations() {
         }}
       />
       <div
-        className="absolute -z-30 top-[73%] bottom-[-42%] lg:top-[129%] lg:bottom-[-177%] left-1/2 -translate-x-1/2 w-full"
+        className="absolute -z-30 top-[73%] bottom-[-42%] lg:top-[129%] lg:bottom-[-82%] left-1/2 -translate-x-1/2 w-full"
         style={{
           background:
             "linear-gradient(180.31deg, #010101 0.28%, #830019 62.26%, #93001C 89.33%)",
