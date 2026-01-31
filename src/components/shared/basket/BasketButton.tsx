42b227106459461caf2f07dd3bfa2f6e43c6fbbe
Shade <kulakaa073@gmail.com>
2026-01-31 03:49:37 +0200

    fix: hover for basket button

--- src/components/shared/basket/BasketButton.tsx
+++ src/components/shared/basket/BasketButton.tsx
@@ -5,7 +5,7 @@ export default function BasketButton() {
   const toggleDrawer = useCartStore((state) => state.toggleDrawer);
   return (
     <button
-      className="relative size-[25px] lg:size-[35px] outline-none ml-auto mb-[10px] cursor-pointer"
+      className="relative size-[25px] lg:size-[35px] outline-none ml-auto mb-[10px] cursor-pointer transition-opacity duration-300 ease-in-out hover:opacity-70 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
       aria-label="Shopping basket"
       onClick={() => toggleDrawer(true)}
     >
